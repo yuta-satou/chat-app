effa263d87d3b2a52dbd6e3eadf9f0c87f474456
yuta-satou <68938732+yuta-satou@users.noreply.github.com>
2020-08-06 17:40:47 +0900

    各モデルのテスト

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe '#create' do
+    before do@user = FactoryBot.build(:user)
+    end
+    it "nameとemail、passwordとpassword_confirmationが存在すれば登録できること" do
+      expect(@user).to be_valid
+    end
+    it "nameが空では登録できないこと" do
+      @user.name = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it "emailが空では登録できないこと" do
+      @user.email = nil
+      @user.valid?
+      # binding.pry
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+
+    it "passwordが空では登録できないこと" do
+      @user.password = nil
+      @user.valid?
+      # binding.pry
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+    it "passwordが存在してもpassword_confirmationが空では登録できないこと" do
+      @user.password_confirmation = ""
+      @user.valid?
+      # binding.pry
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+
+    end
+
+    it "passwordが6文字以上であれば登録できること" do
+      @user.password = "111111"
+      @user.password_confirmation = "111111"
+      expect(@user).to be_valid
+      # binding.pry
+    end
+
+    it "passwordが5文字以下であれば登録できないこと" do
+      @user.password = "11111"
+      @user.password_confirmation = "11111"
+      @user.valid?
+      # binding.pry
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+    end
+    it "重複したemailが存在する場合登録できないこと" do
+      @user.save
+      another_user = FactoryBot.build(:user, email: @user.email)
+      another_user.valid?
+      # binding.pry
+      expect(another_user.errors.full_messages).to include("Email has already been taken")
+    end
+  end
+end
